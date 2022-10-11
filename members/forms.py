93810f11bc9760754d0f0899fa19a24089e8caa9
Hiram Kumado <hiramkumado@gmail.com>
2022-10-11 18:58:25 +0000

    minor change

--- members/forms.py
+++ members/forms.py
@@ -7,21 +7,25 @@ class RegisterUserForm(UserCreationForm):
     email = forms.CharField(max_length=100, widget=forms.TimeInput(
         attrs={'class': 'form-control', 'placeholder': 'Enter your email here!'}))
     first_name = forms.CharField(max_length=30, widget=forms.TimeInput(
-        attrs={'class': 'form-control', 'placeholder': 'First name'}))
+        attrs={'class': 'form-control', 'placeholder': 'Enter your first name!'}))
     last_name = forms.CharField(max_length=30, widget=forms.TimeInput(
-        attrs={'class': 'form-control', 'placeholder': 'Last name'}))
-
-    email.label = ''
-    first_name.label = ''
-    last_name.label = ''
-
-    class Meta:
-        model = User
-        fields = ('username', 'first_name', 'last_name',
-                  'email', 'password1', 'password2')
+        attrs={'class': 'form-control', 'placeholder': 'Enter your last name!'}))
 
     def __init__(self, *args, **kwargs):
         super(RegisterUserForm, self).__init__(*args, **kwargs)
         self.fields['username'].widget.attrs['class'] = 'form-control'
         self.fields['password1'].widget.attrs['class'] = 'form-control'
         self.fields['password2'].widget.attrs['class'] = 'form-control'
+
+        # setting the field labels to empty
+        self.fields['username'].label = ''
+        self.fields['email'].label = ''
+        self.fields['first_name'].label = ''
+        self.fields['last_name'].label = ''
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name',
+                  'email', 'password1', 'password2')
+        widgets = {'username': forms.TextInput(
+            attrs={'placeholder': 'Please enter your username'}), }
